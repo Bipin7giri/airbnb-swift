6e8c7472f981e00a983cf5c2d166477b6b4b7529
bipin7giri <bipingiri27@gmail.com>
2024-03-23 12:26:03 +0545

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Bipin Giri on 23/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
